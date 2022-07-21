4a195f10ff88f8cfac93daf1d85f40a3f534d846
Yago-byte <yagosantanna99@gmail.com>
2022-07-21 13:36:59 -0300

    Continuando o curso

---
+++ Fundamentos/erro.js
@@ -0,0 +1,23 @@
+function tratarErrorElancar(erro){
+   // throw new Error('...')
+   // throw 10
+   // throw true
+   // throw 'mensagem'
+   throw {
+    nome: erro.name,
+    msg: erro.message,
+    date: new Date
+   }
+}
+function imprimirNomeGritado(obj) {
+    try{
+    console.log(obj.name.toUpperCase() + "!!!")    
+    } catch (e){
+        tratarErrorElancar(e)
+    } finally {
+        console.log('final')
+    }
+}
+
+const obj = { name : "Roberto"}
+imprimirNomeGritado(obj)
